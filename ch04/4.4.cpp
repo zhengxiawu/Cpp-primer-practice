e7364ea63489eb8c55117a1d67a6d5bd997513ec
sherwood <zhengxiawu@126.com>
2016-11-22 22:55:41 +0800

    运算符

---
+++ ch04/4.4.cpp
@@ -0,0 +1,21 @@
+/*************************************************************************
+	> File Name: 4.4.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2016年11月22日 星期二 22时10分22秒
+ ************************************************************************/
+
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+using std::cout;
+using std::cin;
+using std::vector;
+using std::string;
+int main()
+{
+    cout<<12/3*4+5*15+24%4/2<<std::endl;
+
+    return 0;
+}
