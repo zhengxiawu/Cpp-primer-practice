2daf284f6ada064f81bcfbfeb84061c9ba7a213f
sherwood <zhengxiawu@126.com>
2016-11-24 22:57:22 +0800

    位运算符

---
+++ ch04/4.28.cpp
@@ -0,0 +1,35 @@
+/*************************************************************************
+	> File Name: 4.28.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2016年11月24日 星期四 22时36分09秒
+ ************************************************************************/
+
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+using std::cout;
+using std::endl;
+using std::cin;
+using std::vector;
+using std::string;
+int main()
+{
+
+    cout<<"int "<<sizeof(int)<<endl;
+    cout<<"short "<<sizeof(short)<<endl;
+    cout<<"long " <<sizeof(long)<<endl;
+    cout<<"char "<<sizeof(char)<<endl;
+    cout<<"wchar_t "<<sizeof(wchar_t)<<endl;
+    cout<<"bool "<<sizeof(bool)<<endl;
+    cout<<"float "<<sizeof(float)<<endl;
+    cout<<"double"<<sizeof(double)<<endl;
+    cout<<"long double"<<sizeof(long double)<<endl;
+    cout<<sizeof(unsigned int)<<endl;
+    cout<<sizeof(unsigned short)<<endl;
+    cout<<sizeof(unsigned long)<<endl;
+    cout<<sizeof(unsigned char)<<endl;
+    cout<<sizeof(unsigned wchar_t)<<endl;
+    return 0;
+}
