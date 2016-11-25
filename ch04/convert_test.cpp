95f9f3a110d60e4fc007f967227201a6921632e9
sherwood <zhengxiawu@126.com>
2016-11-25 22:54:47 +0800

    test

---
+++ ch04/convert_test.cpp
@@ -0,0 +1,23 @@
+/*************************************************************************
+	> File Name: convert_test.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2016年11月25日 星期五 22时22分11秒
+ ************************************************************************/
+
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+using std::cout;
+using std::endl;
+using std::cin;
+using std::vector;
+using std::string;
+int main()
+{
+    int i=1;
+    double j = 2.0;
+    cout<<i/j<<endl;
+    return 0;
+}
