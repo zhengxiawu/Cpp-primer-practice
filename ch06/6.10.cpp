b56bb9651e7a2f2333661af2305455143ed91aa0
sherwood <zhengxiawu@126.com>
2016-12-03 22:39:27 +0800

    形参

---
+++ ch06/6.10.cpp
@@ -0,0 +1,30 @@
+/*************************************************************************
+	> File Name: 6.10.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2016年12月03日 星期六 22时31分27秒
+ ************************************************************************/
+
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+using std::cout;
+using std::endl;
+using std::cin;
+using std::vector;
+using std::string;
+void swap(int *p1,int *p2)
+{
+    int temp = *p1;
+    *p1 = *p2;
+    *p2 = temp;
+}
+int main()
+{
+    int i1 = 1,i2 = 2;
+    cout<<"befor change:"<<i1<<":"<<i2<<endl;
+    swap(&i1,&i2);
+    cout<<"after change:"<<i1<<":"<<i2<<endl;
+    return 0;
+}
